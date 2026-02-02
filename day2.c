16149613279e001ebd61e30c44a3e50f3594d94e
manasvvi2008 <manasvvi52@gmail.com>
2026-02-02 15:45:30 +0530

    Add files via upload

---
+++ day2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() 
+{
+    int n, pos;
+    int arr[100]; 
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter position to delete (1-based): ");
+    scanf("%d", &pos);
+    for(int i = pos - 1; i < n - 1; i++) 
+    {
+        arr[i] = arr[i + 1];
+    }
+    printf("Updated array:\n");
+    for(int i = 0; i < n - 1; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
